5d6a8239bd55cb1d3e478cc0526d90e73867943e
Jose Duarte <joseduarte019@gmail.com>
2025-05-16 22:03:10 +0200

    add allowed fingerprints

--- src/utils/allowedFingerprints.js
+++ src/utils/allowedFingerprints.js
@@ -1,4 +1,6 @@
 export const allowedFingerprints = [
-  "abc123fingerprint1",
-  "def456fingerprint2",
+  "fa2053765f6db8b6b133b5ad70693441",
+  "1f759db9ca5e19e91963562b845271f5",
+  "ef24d4f463a9e6a416448340cc7d1966",
+  "45ed7bcf98411ee0dbf8ebd9012c790d"
 ];
