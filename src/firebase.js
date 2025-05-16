e55587a9e58803de2dd83ed54d76403612c7a6b7
Jose Duarte <joseduarte019@gmail.com>
2025-05-16 21:38:23 +0200

    use .env firebase variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,13 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBA5aSMnx7-OgeYllpVSLRTTlPmdCmWQvQ",
-  authDomain: "acomerexpresstesting.firebaseapp.com",
-  projectId: "acomerexpresstesting",
-  storageBucket: "acomerexpresstesting.firebasestorage.app",
-  messagingSenderId: "983807315528",
-  appId: "1:983807315528:web:3ebed6b77882e66eb0927d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
